148a4d4e91164e88f98ef2a074aac947793ff2d5
Dipanjan Das <dipanjandas@Dipanjans-MacBook-Air.local>
2024-05-11 01:53:54 +0530

    Creating Spring Project

---
+++ explorespring/src/main/java/com/example/es/explorespring/ExplorespringApplication.java
@@ -0,0 +1,13 @@
+package com.example.es.explorespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorespringApplication.class, args);
+	}
+
+}
